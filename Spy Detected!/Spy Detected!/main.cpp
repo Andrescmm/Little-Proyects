34a6dd3ab610882b2aac6dec6dff65821f27fbef
Andres Cusirramos Marquez Mares <andres.cusirramos@icloud.com>
2021-06-14 18:39:29 -0500

    1

---
+++ Spy Detected!/Spy Detected!/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+int spyDetected(int *arr){
+    
+    
+    return 0;
+}
+
+
+int main() {
+    
+    int testCase , nInt;
+    
+    cin>>testCase;
+    
+    
+    for(int i = 0; i<testCase; i++){
+        cin>>nInt;
+        int *arr=new int[nInt];
+        arr.push_back();
+        spyDetected(arr);
+    }
+   
+}

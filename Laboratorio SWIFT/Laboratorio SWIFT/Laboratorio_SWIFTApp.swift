3e1ce73eba70948649914e1e7af3b60d648fec0c
Andres Cusirramos Marquez Mares <andres.cusirramos@icloud.com>
2021-10-25 18:32:18 -0500

    app practice

---
+++ Laboratorio SWIFT/Laboratorio SWIFT/Laboratorio_SWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorio_SWIFTApp.swift
+//  Laboratorio SWIFT
+//
+//  Created by Andres  on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorio_SWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Laboratorio SWIFT/Laboratorio SWIFTTests/Laboratorio_SWIFTTests.swift
@@ -0,0 +1,33 @@
+//
+//  Laboratorio_SWIFTTests.swift
+//  Laboratorio SWIFTTests
+//
+//  Created by Andres  on 25/10/21.
+//
+
+import XCTest
+@testable import Laboratorio_SWIFT
+
+class Laboratorio_SWIFTTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

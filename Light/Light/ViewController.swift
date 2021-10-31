cf85e9f991a9bee21879767479096785a1fc58e6
Andres Cusirramos Marquez Mares <andres.cusirramos@icloud.com>
2021-10-30 22:00:53 -0500

    1.0

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var ligthOn = true
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUi()
     }
 
-
+    // Function
+    fileprivate func updateUi() {
+        if ligthOn{
+            ///lightButton.setTitle("Off", for: .normal)
+            view.backgroundColor = .white
+        }
+        else{
+            ///lightButton.setTitle("On", for: .normal)
+            view.backgroundColor = .black
+        }
+    }
+    
+    // Button Action
+    @IBAction func buttonPressed(_ sender: Any) {
+        ligthOn.toggle()
+        updateUi()
+    }
+    
 }
 
